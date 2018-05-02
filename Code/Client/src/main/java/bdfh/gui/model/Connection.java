b348490be123a5a2ae84acacb1b57b86165a8061
François <francois.burgener@hotmail.fr>
2018-05-02 15:09:17 +0200

    Deconnexion

--- Code/Client/src/main/java/bdfh/gui/model/Connection.java
+++ Code/Client/src/main/java/bdfh/gui/model/Connection.java
@@ -1,10 +1,15 @@
 package bdfh.gui.model;
 
+import bdfh.net.client.Client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static java.lang.System.exit;
 
 public class Connection extends Application {
 	
@@ -14,6 +19,14 @@ public class Connection extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("/view/loginRegister.fxml"));
 		primaryStage.setTitle("Cheseaux-Poly");
 		primaryStage.setScene(new Scene(root, 650, 400));
+		primaryStage.setOnCloseRequest(event -> {
+			try {
+				Client.getInstance().disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			exit(0);
+		});
 		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
