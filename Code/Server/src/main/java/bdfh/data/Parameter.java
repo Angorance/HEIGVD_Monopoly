de32b357ec38d62429e31872387ce71ef288f279
Helena <helena.reymond@bluewin.ch>
2018-05-04 12:45:48 +0200

    Object Parameter created and implemented

---
+++ Code/Server/src/main/java/bdfh/data/Parameter.java
@@ -0,0 +1,60 @@
+package bdfh.data;
+
+/**
+ * Class used to create parameters linked to one game and to
+ * store the limits of the game fixed by the administrator.
+ *
+ * @author Héléna Line Reymond
+ * @version 1.0
+ */
+public class Parameter {
+
+	public static int minDice;                      // Minimum number of dice allowed
+	public static int maxDice;                      // Maximum number of dice allowed
+	public static int minMoneyAtTheStart;           // Minimum money allowed at the start per player
+	public static int maxMoneyAtTheStart;           // Maximum money allowed at the start per player
+	public static boolean randomGameGeneration;     // True if random game generation allowed, false otherwise
+	
+	public int nbrDice;                             // Number of dice in the game
+	public int moneyAtTheStart;                     // Money given at the start
+	public boolean randomGeneration;                // True if random game generation allowed, false otherwise
+	
+	/**
+	 * Set the parameter of the game
+	 * @param nbrDice               Number of dice in the game
+	 * @param moneyAtTheStart       Money given at the start
+	 */
+	Parameter(int nbrDice, int moneyAtTheStart) {
+		this(nbrDice, moneyAtTheStart, false);
+	}
+	
+	/**
+	 * Set the parameter of the game
+	 * @param nbrDice               Number of dice in the game
+	 * @param moneyAtTheStart       Money given at the start
+	 * @param randomGeneration      True if random game generation allowed, false otherwise
+	 */
+	Parameter(int nbrDice, int moneyAtTheStart, boolean randomGeneration) {
+		this.nbrDice = nbrDice;
+		this.moneyAtTheStart = moneyAtTheStart;
+		this.randomGeneration = randomGeneration;
+	}
+	
+	/**
+	 * Set the limits of parameters for all game created
+	 * @param minDice                   Minimum number of dice allowed
+	 * @param maxDice                   Maximum number of dice allowed
+	 * @param minMoney                  Minimum money allowed at the start per player
+	 * @param maxMoney                  Maximum money allowed at the start per player
+	 * @param randomGameGeneration      True if randomGameGeneration allowed, false otherwise
+	 */
+	public static void updateLimits(int minDice, int maxDice, int minMoney, int maxMoney, boolean randomGameGeneration){
+		
+		// TODO - call this method at the connection of the server and each time a player want to create a game
+		Parameter.minDice = minDice;
+		Parameter.maxDice = maxDice;
+		Parameter.minMoneyAtTheStart = minMoney;
+		Parameter.maxMoneyAtTheStart = maxMoney;
+		Parameter.randomGameGeneration = randomGameGeneration;
+	}
+}
