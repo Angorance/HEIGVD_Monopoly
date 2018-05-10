3522a5daa1586a6529e10a5f1ee1a46e8eb541f4
Daniel Gonzalez Lopez <idaniel.gonzalezlopezi@gmail.com>
2018-05-10 14:55:01 +0200

    Added BoundParameters for serialisation

--- Code/Server/src/main/java/bdfh/serializable/BoundParameters.java
+++ Code/Server/src/main/java/bdfh/serializable/BoundParameters.java
@@ -7,7 +7,12 @@ package bdfh.serializable;
  */
 public class BoundParameters {
 	
-	private int min
+	public int minDice;                      // Minimum number of dice allowed
+	public int maxDice;                      // Maximum number of dice allowed
+	public int minMoneyAtTheStart;           // Minimum money allowed at the start per player
+	public int maxMoneyAtTheStart;           // Maximum money allowed at the start per player
+	public boolean randomGameGeneration;     // True if random game generation allowed, false otherwise
+	
 	
 	private BoundParameters() {}
 	
@@ -19,5 +24,56 @@ public class BoundParameters {
 	public static BoundParameters getInstance() {
 		
 		return Instance.instance;
+	}
+	
+	
+	public int getMinDice() {
+		
+		return minDice;
+	}
+	
+	public int getMaxDice() {
+		
+		return maxDice;
+	}
+	
+	public int getMinMoneyAtTheStart() {
+		
+		return minMoneyAtTheStart;
+	}
+	
+	public int getMaxMoneyAtTheStart() {
+		
+		return maxMoneyAtTheStart;
+	}
+	
+	public boolean isRandomGameGeneration() {
+		
+		return randomGameGeneration;
+	}
+	
+	public void setMinDice(int minDice) {
+		
+		this.minDice = minDice;
+	}
+	
+	public void setMaxDice(int maxDice) {
+		
+		this.maxDice = maxDice;
+	}
+	
+	public void setMinMoneyAtTheStart(int minMoneyAtTheStart) {
+		
+		this.minMoneyAtTheStart = minMoneyAtTheStart;
+	}
+	
+	public void setMaxMoneyAtTheStart(int maxMoneyAtTheStart) {
+		
+		this.maxMoneyAtTheStart = maxMoneyAtTheStart;
+	}
+	
+	public void setRandomGameGeneration(boolean randomGameGeneration) {
+		
+		this.randomGameGeneration = randomGameGeneration;
 	}
 }
