993d3b75e05748597cb26f7587862b2f18b796b2
François <francois.burgener@hotmail.fr>
2018-05-25 09:00:24 +0200

    correction detailLobby

--- Code/Client/src/main/java/bdfh/gui/controller/Controller_lobbyList.java
+++ Code/Client/src/main/java/bdfh/gui/controller/Controller_lobbyList.java
@@ -154,7 +154,8 @@ public class Controller_lobbyList implements Initializable {
 		Label[] labelsPlayers = { name_player1, name_player2, name_player3, name_player4 };
 		Label[] labelsReadys = { ready_player1, ready_player2, ready_player3, ready_player4 };
 		
-		for(int i = 0; i < players.size(); ++i){
+		System.out.println(players);
+		for(int i = 0; i < labelsPlayers.length; ++i){
 			labelsPlayers[i].setText("-");
 			labelsReadys[i].setText("-");
 		}
