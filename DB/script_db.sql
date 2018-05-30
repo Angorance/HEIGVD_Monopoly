d0e4ee5e928b648d680c5b548578f851f7a99084
Helena <helena.line.reymond@gmail.com>
2018-05-30 09:35:00 +0200

    Script DB updated for prices and squares

--- DB/script_db.sql
+++ DB/script_db.sql
@@ -189,8 +189,70 @@ INSERT INTO `cheseaux-poly`.`card` (`id`, `text`, `quantity`, `action_id`) VALUE
 -- -----------------------------------------------------
 -- Create the prices
 -- -----------------------------------------------------
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 1, 2, 60, 50, 50, 30);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 2, 4, 60, 50, 50, 30);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 3, 6, 100, 50, 50, 50);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 4, 8, 120, 50, 50, 60);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 5, 10, 140, 100, 100, 70);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 6, 12, 160, 100, 100, 80);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 7, 14, 180, 100, 100, 90);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 8, 16, 200, 100, 100, 100);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 9, 18, 200, 150, 150, 110);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 10, 20, 240, 150, 150, 120);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 11, 22, 260, 150, 150, 130);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 12, 24, 280, 150, 150, 140);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 13, 26, 300, 200, 200, 150);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 14, 28, 320, 200, 200, 160);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 15, 35, 350, 200, 200, 175);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 16, 50, 400, 200, 200, 200);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 17, 25, 200, NULL, NULL, 100);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 18, 200, NULL, NULL, NULL, NULL);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 19, 4, 150, NULL, NULL, 75);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 20, 50, NULL, NULL, NULL, NULL);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 21, 18, 220, 150, 150, 110);
+INSERT INTO `cheseaux-poly`.`price` (`id`, `rent`, `price`, `priceHouse`, `priceHotel`, `hypothec`) VALUES ( 22, 100, NULL, NULL, NULL, NULL);
 
 
 -- -----------------------------------------------------
 -- Create the squares
 -- -----------------------------------------------------
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (1, 0, "START", "Départ", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (2, 1, "BROWN", "F01", 1);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (3, 2, "CARD", "Carte chance", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (4, 3, "BROWN", "A01", 2);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (5, 4, "TAX", "Frais d'écolage", 18);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (6, 5, "INSTITUTE", "REDS", 17);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (7, 6, "CYAN", "Zone casier", 3);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (8, 7, "CARD", "Carte chance", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (9, 8, "CYAN", "Table de ping pong", 3);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (10, 9, "CYAN", "Micro-onde", 4);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (11, 10, "EXAM", "Salle d'examen", 20);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (12, 11, "PINK", "Secrétariat", 5);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (13, 12, "COMPANY", "Selecta", 19);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (14, 13, "PINK", "Biblio", 5);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (15, 14, "PINK", "FabLab", 6);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (16, 15, "INSTITUTE", "SIM", 17);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (17, 16, "ORANGE", "Reprographie", 7);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (18, 17, "CARD", "Carte chance", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (19, 18, "ORANGE", "Gaps", 7);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (20, 19, "ORANGE", "AGE", 8);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (21, 20, "BREAK", "Terrasse", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (22, 21, "RED", "Palmeraie", 9);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (23, 22, "CARD", "Carte chance", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (24, 23, "RED", "Orangeraie", 21);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (25, 24, "RED", "Service informatique", 10);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (26, 25, "INSTITUTE", "MNT", 17);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (27, 26, "YELLOW", "C23", 11);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (28, 27, "YELLOW", "B23", 11);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (29, 28, "COMPANY", "Imprimante", 19);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (30, 29, "YELLOW", "Aula", 12);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (31, 30, "GO_EXAM", "Allez en prison!", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (32, 31, "GREEN", "J01", 13);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (33, 32, "GREEN", "H01", 13);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (34, 33, "CARD", "Carte chance", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (35, 34, "GREEN", "G01", 14);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (36, 35, "INSTITUTE", "IICT", 17);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (37, 36, "CARD", "Carte chance", NULL);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (38, 37, "BLUE", "Parking", 15);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (39, 38, "TAX", "Baleinev", 22);
+INSERT INTO `cheseaux-poly`.`square` (`id`, `position`, `family`, `name`, `price_id`) VALUES (40, 39, "BLUE", "Chill out", 16);
